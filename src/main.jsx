83c83061e70c2a2cbde210299a02f4ea1a6c5246
wally720 <wally.spm@hotmail.com>
2025-12-21 12:04:27 -0500

    replace BrowserRouter with HashRouter and wrap App with VehicleProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from './App.jsx'
 import './index.css'
 // 1. Cambia la importación de BrowserRouter a HashRouter
 import { HashRouter } from 'react-router-dom' 
+import { VehicleProvider } from './context/VehicleContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* 2. Envuelve tu App con HashRouter en lugar de BrowserRouter */}
     <HashRouter>
-      <App />
+      <VehicleProvider>
+        <App />
+      </VehicleProvider>
     </HashRouter>
   </React.StrictMode>,
 )
